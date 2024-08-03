1cd7f28d816b3425262f3f1487e5ba7739ce18b8
StephenZR03 <steveoncaffeine@gmail.com>
2024-08-03 21:02:26 +0100

    complete crud functionality

--- script.js
+++ script.js
@@ -17,7 +17,14 @@ class Book {
     }
 
     static deleteEntry(n) {
-        if (n - 1 > -1) this.#entries.splice(n - 1, 1)
+        if (n > -1) this.#entries.splice(n, 1)
+    }
+
+    static editEntry(index, props) {
+        this.#entries[index] = {
+            ...this.#entries[index],
+            ...props,
+        }
     }
 }
 
@@ -25,71 +32,120 @@ const makeEntryFormContainer = document.querySelector('.form-container.create');
 const editEntryFormContainer = document.querySelector('.form-container.update');
 const libList = document.querySelector('main');
 const makeEntryButton = document.querySelector('.make-entry button');
-const cancelEntryMenuButton = document.querySelector('.create-form button');
-const cancelEditMenuButton = document.querySelector('.update-form button');
+const cancelEntryMenuButton = document.querySelector('.create-form .cancel');
+const cancelEditMenuButton = document.querySelector('.update-form .cancel');
 const addToLibrary = document.querySelector('.create .submit');
+const saveEdits = document.querySelector('.update .submit');
 
 function showMenu(event) {
     if (event.target.parentNode.classList.contains('make-entry')) {
         makeEntryFormContainer.style.display = 'flex';
     } else {
         editEntryFormContainer.style.display = 'flex';
+        editEntryFormContainer.dataset.number = Number(event.target.parentNode.dataset.number);
     }
 }
 
 function hideMenu(event) {
-    event.target.parentNode.style.display = 'none';
+    event.target.parentNode.parentNode.style.display = 'none';
 }
 
 function makeEntryDOM() {
-    const title = document.querySelector('.create .title').value;
-    const author = document.querySelector('.create .author').value;
-    const length = document.querySelector('.create .length').value;
-    const genre = document.querySelector('.create .genre').value;
-    const status = document.querySelector('input[name=status]:checked').value;
-    Book.makeEntry(title, author, length, genre, status);
+    let addTitle = document.querySelector('.create .title');
+    let addAuthor = document.querySelector('.create .author');
+    let addLength = document.querySelector('.create .length');
+    let addGenre = document.querySelector('.create .genre');
+    let addStatus = document.querySelector('.create input[name=status]:checked');
+    Book.makeEntry(addTitle.value, addAuthor.value, addLength.value, addGenre.value, addStatus.value);
     makeEntryFormContainer.style.display = 'none';
+    addTitle.value = '';
+    addAuthor.value = '';
+    addLength.value = '';
+    addGenre.value = '';
+    let statusChecks = addStatus.parentNode.parentNode.querySelectorAll('input[name=status]');
+    for (let i = 0; i < statusChecks.length; i++) {
+        statusChecks[i].checked = false;
+    }
+    displayEntries();
+}
+
+function editEntryDOM(event) {
+    let editTitle = document.querySelector('.update .title');
+    let editAuthor = document.querySelector('.update .author');
+    let editLength = document.querySelector('.update .length');
+    let editGenre = document.querySelector('.update .genre');
+    let editStatus = document.querySelector('.update input[name=status]:checked');
+
+    const index = Number(event.target.parentNode.parentNode.dataset.number);
+    const props = {}
+    editTitle.value ? props.title = editTitle.value : null;
+    editAuthor.value ? props.author = editAuthor.value : null;
+    editLength.value ? props.length = editLength.value : null;
+    editGenre.value ? props.genre = editGenre.value : null;
+    editStatus ? props.status = editStatus.value : null;
+    Book.editEntry(index, props)
+    editTitle.value = '';
+    editAuthor.value = '';
+    editLength.value = '';
+    editGenre.value = '';
+    editStatus.ariaChecked = false;
+    editEntryFormContainer.style.display = 'none';
+    let statusChecks = editStatus.parentNode.parentNode.querySelectorAll('input[name=status]');
+    for (let i = 0; i < statusChecks.length; i++) {
+        statusChecks[i].checked = false;
+    }
+    displayEntries();
+    displayEntries();
+}
+
+function deleteEntryDOM(event) {
+    Book.deleteEntry(Number(event.target.parentNode.dataset.number));
     displayEntries();
 }
 
 function displayEntries() {
+    const bookRows = document.querySelectorAll('.entry');
+    for (let i = 0; i < bookRows.length; i++) {
+        libList.removeChild(bookRows[i]);
+    }
+
     Book.getEntries().forEach((book, index) => {
-        let container = document.createElement("div");
+        const container = document.createElement("div");
         container.classList.add("row", "entry");
         container.dataset.number = index;
 
-        let title = document.createElement("div");
+        const title = document.createElement("div");
         title.textContent = `${book.title}`
         container.appendChild(title);
 
-        let author = document.createElement("div");
+        const author = document.createElement("div");
         author.textContent = `${book.author}`
         container.appendChild(author);
 
-        let length = document.createElement("div");
+        const length = document.createElement("div");
         length.textContent = `${book.length}`
         container.appendChild(length);
 
-        let genre = document.createElement("div");
+        const genre = document.createElement("div");
         genre.textContent = `${book.genre}`
         container.appendChild(genre);
 
-        let status = document.createElement("div");
+        const status = document.createElement("div");
         status.textContent = `${book.status}`;
         container.appendChild(status);
 
-        let editEntryButton = document.createElement("button");
+        const editEntryButton = document.createElement("button");
         editEntryButton.classList.add("edit-entry");
-        editEntryButton.textContent = "edit entry";
+        editEntryButton.textContent = "edit";
         editEntryButton.dataset.number = index;
-        // editEntryButton.addEventListener("click", n);
+        editEntryButton.addEventListener("click", showMenu);
         container.appendChild(editEntryButton);
 
-        let deleteEntryButton = document.createElement("button");
+        const deleteEntryButton = document.createElement("button");
         deleteEntryButton.classList.add("remove-book-button");
-        deleteEntryButton.textContent = "Remove Book";
+        deleteEntryButton.textContent = "remove";
         deleteEntryButton.dataset.number = index;
-        // deleteEntryButton.addEventListener("click", deleteEntry);
+        deleteEntryButton.addEventListener("click", deleteEntryDOM);
         container.appendChild(deleteEntryButton);
 
         libList.appendChild(container);
@@ -100,3 +156,4 @@ makeEntryButton.addEventListener('click', showMenu);
 cancelEntryMenuButton.addEventListener('click', hideMenu);
 cancelEditMenuButton.addEventListener('click', hideMenu);
 addToLibrary.addEventListener('click', makeEntryDOM);
+saveEdits.addEventListener('click', editEntryDOM);
